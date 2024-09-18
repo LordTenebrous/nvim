96df43ad1bbb15b640544f370a4074943677d89d
SimonMoser22 <120651090+SimonMoser22@users.noreply.github.com>
2024-09-18 13:21:59 +0200

    lsp-config.lua uses nvim native lspconfig
    Remove Mason lsp-config setup and setup clangd with nvim lspconfig

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,36 +1,3 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "clangd", "marksman" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-                capabilities = capabilities
-            })
-            lspconfig.clangd.setup({
-                capabilities = capabilities
-            })
-            lspconfig.marksman.setup({
-                capabilities = capabilities
-            })
-			vim.keymap.set("n", "I", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+	require'lspconfig'.clangd.setup{}
 }
