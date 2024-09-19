6915d7ee7596d4ac868ac73f9fa43deb86f532ab
Simon Moser <simon.moser22.sm@gmail.com>
2024-09-19 06:55:16 +0200

    Well now clangd works :)
    It appears I deleted too much xD

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,7 +3,27 @@ local os_name = vim.loop.os_uname().sysname
 if os_name == "FreeBSD" then
 return {
   -- FreeBSD specific configuration
-    require('lspconfig').clangd.setup{}
+  -- require('lspconfig').clangd.setup{}
+  {
+		"neovim/nvim-lspconfig",
+		config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.clangd.setup({
+                capabilities = capabilities
+            })
+            lspconfig.marksman.setup({
+                capabilities = capabilities
+            })
+			vim.keymap.set("n", "I", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
 else
 return {
