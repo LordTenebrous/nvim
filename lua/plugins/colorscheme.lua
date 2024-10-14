6e8ae7b0f960c1bc12e906f2eaabc2e6be2ee6d0
Leon Wandruschka <leon.wandruschka@student.tugraz.at>
2024-10-14 20:11:32 +0200

    Change colorscheme and add todo-comments

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,18 @@
 return {
-    {
-        'ful1e5/onedark.nvim',
-        name = "onedark",
-        priority = 1000,
-        config = function()
-            vim.cmd.colorscheme("onedark")
-        end,
-    },
+  {
+    'ful1e5/onedark.nvim',
+    name = "onedark",
+    priority = 1000,
+    --config = function()
+    --  vim.cmd.colorscheme("onedark")
+    --end,
+  },
+  {
+    "catppuccino/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("catppuccin-macchiato")
+    end,
+  },
 }
