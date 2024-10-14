d53c326cd02f96b4f625a95f264808c81cafd171
Leon Wandruschka <leon.wandruschka@student.tugraz.at>
2024-10-14 20:40:15 +0200

    Set the nvimtree window to hover and remove lualine from it

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -6,8 +6,35 @@ return {
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
-		require("nvim-tree").setup({})
+		require("nvim-tree").setup({
+			view = {
+				-- Set the name of the NvimTree window to "NvimTree"
+				float = {
+					enable = true,
+					open_win_config = {
+						border = "rounded",  -- Optional, for aesthetics
+					},
+				},
+			},
+			renderer = {
+				icons = {
+					show = {
+						file = true,
+						folder = true,
+						folder_arrow = true,
+						git = true,
+					},
+				},
+			},
+		})
+
+		-- Remove the "Lua" label from the window (optional)
+		vim.api.nvim_set_hl(0, 'NvimTreeNormal', { bg = 'none' })
+		vim.opt.winbar = nil  -- Remove the winbar if using it
+
+		-- Key mappings
 		vim.keymap.set({ "n", "i", "v" }, "<c-b>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 		vim.keymap.set({ "n", "i", "v" }, "<c-f>", "<Esc>:NvimTreeFocus<CR>", { noremap = true, silent = true })
 	end,
 }
+
