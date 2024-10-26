2e69bdf450dd6f027a41c566d84ee7cbdc389108
Leon Wandruschka <leon.wandruschka@student.tugraz.at>
2024-10-26 23:58:50 +0200

    Add Lazygit and set new terminal esc sequence

--- lua/core/commands.lua
+++ lua/core/commands.lua
@@ -1,6 +1,12 @@
--- Mapping Escape to switch from terminal mode to normal mode
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', {noremap = true, silent = true})
-
--- Mapping :q! in terminal mode to switch to normal mode and then quit
-vim.keymap.set('t', ':q!', '<C-\\><C-n>:q!<CR>', {noremap = true, silent = true})
-
+-- Keybinding to get in normal mode in normal terminal and use esc to move in lazygit
+vim.api.nvim_create_autocmd("TermEnter", {
+	callback = function()
+		-- If the terminal window is lazygit, we do not make changes to avoid clashes
+		if string.find(vim.api.nvim_buf_get_name(0), "lazygit") then
+			return
+		end
+		vim.keymap.set("t", "<ESC>", function()
+			vim.cmd("stopinsert")
+		end, { buffer = true })
+	end,
+})
