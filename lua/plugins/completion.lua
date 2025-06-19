389277025c6a700536eaf1e0b0189205401d849d
LordTenebrous <danielmorenoperez836@gmail.com>
2025-06-19 13:53:43 +0200

    Ctrl+c for Copilot

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -5,7 +5,7 @@ return {
     {
         "github/copilot.vim",
         config = function()
-            vim.keymap.set('i', '<Right>', 'copilot#Accept("\\<CR>")', {
+            vim.keymap.set('i', '<C-c>', 'copilot#Accept("\\<CR>")', {
                 expr = true,
                 replace_keycodes = false
             })
